6a29b93f479ab14c853df749b62fcba9b71df5c5
MartinGK <mrtgeka@gmail.com>
2020-08-03 17:40:17 -0300

    types

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,10 +1,18 @@
-function add(n1: number, n2: number){
-    return n1 + n2;
+function add(n1: number, n2: number, printResult: boolean, text: string) {
+
+    const result = n1 + n2;
+    if (printResult) {
+        return text + result;
+    } else {
+        return result;
+    }
 }
 
 const number1 = 5;
 const number2 = 2.8;
+const printResult = true;
+const text = 'Result is: '
 
-const result = add(number1, number2);
+const result = add(number1, number2, printResult, text);
 
 console.log(result)
